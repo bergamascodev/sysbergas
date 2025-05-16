ba1715fa89b96de4bb01162f376f5100ace48d11
gabriel <bergamasco.gabriel@hotmail.com>
2025-05-16 15:04:16 -0300

    commit inicial

---
+++ src/main/java/com/sysbergas/SysbergasApplication.java
@@ -0,0 +1,13 @@
+package com.sysbergas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysbergasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysbergasApplication.class, args);
+	}
+
+}
